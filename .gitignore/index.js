9e8f29fcbb49bb4fadfec0e9b130208ce82e27f6
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-06-11 18:05:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-emitter.setMaxListeners('9999999999999999999999999999999999999999999999999999999999999999999999999')
+emitter.setMaxListeners(9999)
 
 client.on('ready', () => {
   client.user.setPresence({ game: { name: '[m!help]', type: 0}})
