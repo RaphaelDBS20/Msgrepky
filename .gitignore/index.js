ec55470521481026db63fb35eb6ac5e3badc1dfc
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-06-11 18:24:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const emitter = require('events').EventEmitter.prototype._maxListeners = 100;
 const emitter = new EventEmitter()
 emitter.setMaxListeners(100)
 
