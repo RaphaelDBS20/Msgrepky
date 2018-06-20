393b1bd40b532cd8bc276f05d041fc07f57559da
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-06-20 20:08:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -163,4 +163,10 @@ client.on('message', msg => {
   }
 })
 
+client.on('message', msg => {
+  if (msg.content === 'ai') {
+    msg.channel.send('ai');
+  }
+});
+
 client.login(process.env.loginuser)
