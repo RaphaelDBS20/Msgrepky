b66feb5aa89fda7ccdbf1a85bb078af42aa20214
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-06-23 19:39:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -199,4 +199,10 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', msg => {
+  if (msg.content === 'm!about') {
+    msg.channel.send('Bot héberger sur github et émuler par heroku bot coder en : javascript (node.js) la version du bot : b00008');
+  }
+});
+
 client.login(process.env.loginuser)
