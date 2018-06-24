57e0fa7ddcc55700645b6db225d892e4a65118cf
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-06-24 15:49:18 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -210,18 +210,4 @@ client.on('message', message => {
   // we ignore it
   if (!message.guild) return;
 
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
-
 client.login(process.env.loginuser)
