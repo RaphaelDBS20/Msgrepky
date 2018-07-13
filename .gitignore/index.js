8d89f8cbc2c090d6bba9e9e445c429b3a52df9de
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-07-13 18:19:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -212,7 +212,7 @@ client.on('message', msg => {
 });
 
 client.on('message', message => {
-  if (message.content.startWith("m!sondage")) {
+  if (message.content.startsWith("m!sondage")) {
     if(message.author.id ==  "330594064414146560"){
       let args = message.content.split(" ").slice(1);
       let thingToEcho = args.join(" ")
