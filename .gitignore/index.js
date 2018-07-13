0e731889e909bde899ba26deb122f15253ebd038
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-07-13 18:47:17 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -223,8 +223,8 @@ client.on('message', message => {
        .setTimestamp()
       message.guild.channels.find("name", "sondages").sendEmbed(embed)
       .then(function (message) {
-          message.react("✔")
-          message.react("✖")
+          message.react("✅")
+          message.react("❌")
     }).catch(function() {
     });
     }else{
