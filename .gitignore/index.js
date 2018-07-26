9926dbce6af23504e6d98867c7c7d4e3b4711df6
RaphaelDBS20 <39079524+RaphaelDBS20@users.noreply.github.com>
2018-07-26 21:19:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,12 +11,12 @@ client.on('ready', () => {
 
 client.on("guildMemberAdd", member => {
     let role = member.guild.roles.find("name", "GENS")
-    member.guild.channels.find("name", "géneral").send(`:video_game: Bienvenue a toi **${member.user.username}** je te consseil de lire les régles avant de continuer`)
+    member.guild.channels.find("name", "📝-géneral").send(`:video_game: Bienvenue a toi **${member.user.username}** je te consseil de lire les régles avant de continuer`)
     member.addRole(role)
 })
 
 client.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "géneral").send(`:space_invader: **${member.user.username}** a quitté le server au revoir...`)
+    member.guild.channels.find("name", "📝-géneral").send(`:space_invader: **${member.user.username}** a quitté le server au revoir...`)
 })
 
 client.on('message', msg => {
